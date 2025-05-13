d8bb270b37d115063a001195cb4713515a610d55
programacionparaaprender <yancel209@gmail.com>
2025-05-13 13:20:18 -0500

    agregando cambios

--- src/main/java/com/program/app/JavaGofApplication.java
+++ src/main/java/com/program/app/JavaGofApplication.java
@@ -1,10 +1,14 @@
 package com.program.app;
 
+import java.math.BigDecimal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.program.app.config.ConfigurationService;
+import com.program.app.interfaz.PaymentMethod;
 import com.program.app.interfaz.impl.PaymentService;
+
 
 @SpringBootApplication
 public class JavaGofApplication {
@@ -12,6 +16,7 @@ public class JavaGofApplication {
 	public static void main(String[] args) {
 		
 		principio_creacional();
+		principio_factory_method();
 		//SpringApplication.run(JavaGofApplication.class, args);
 	}
 	
@@ -23,8 +28,10 @@ public class JavaGofApplication {
 	
 	private static void principio_factory_method() {
 		PaymentService service = new PaymentService();
-		System.out.println("principio de creación Singleton");
-		System.out.println(service.getConfig("app.version"));
+		PaymentMethod creditcard = service.createPaymentMethod("creditcard");
+		System.out.println("principio factory method");
+		BigDecimal amount = new BigDecimal("123.45");
+		creditcard.processPayment(amount);
 	}
 
 }
